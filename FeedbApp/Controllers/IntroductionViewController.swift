758d51deec5385834767978d6efed8271eac9387
Reivaj Gómez <informaticorp80@gmail.com>
2023-03-21 23:23:54 -0600

    Introduction screen now ready although disconnected

---
+++ FeedbApp/Controllers/IntroductionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  IntroductionViewController.swift
+//  FeedbApp
+//
+//  Created by Reivaj Gomez on 21/03/23.
+//
+
+import UIKit
+
+final class IntroductionViewController: UIViewController {
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  
+  }
+  
+}
